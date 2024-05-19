03816fcf7425b1b2cde68a6c6b349eada1d14ec5
mabytax2 <117278383+mabytax2@users.noreply.github.com>
2024-05-19 20:33:24 -0300

    eliminacion del css de mediaqueries que esta en el css inicial, creacion de pagina espejo de contacto para agregar la validacion de datos con  javascript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,12 @@
-<script src="https://kit.fontawesome.com/28d049d17c.js" crossorigin="anonymous"></script>
+{/* <script src="https://kit.fontawesome.com/28d049d17c.js" crossorigin="anonymous"></script> */}
+// function validarFormulario(){
+//     let nombre=document.getElementById("name").value;
+//     let correoe=document.getElementById("email").value;
+// document.write(nombre);
+// document.write(correoe);
+//     if(nombre==""||correoe==""){
+//         alert("Por favor, no dejes campos vacios!")
+//         return false;
+//     }
+//      return true;   
+// }
