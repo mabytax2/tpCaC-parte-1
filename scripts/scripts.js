a14d0b28df188e77be71a9b7831b310f20bec4da
mabytax2 <117278383+mabytax2@users.noreply.github.com>
2024-05-19 06:05:09 -0300

    mejoras en contacto

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,1 @@
 <script src="https://kit.fontawesome.com/28d049d17c.js" crossorigin="anonymous"></script>
-<script>
-{/* if(name.lengh===0)
-alert("No puedes dejar el campo vacio");
-if(mail.lengh) */}
-</script>
