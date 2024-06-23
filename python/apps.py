5788ddefcd168a703cda2a38c7a13487a8df682e
mabytax2 <117278383+mabytax2@users.noreply.github.com>
2024-06-23 05:40:49 -0300

    creando carpeta templates, formularios asociados y archivos de apps

--- python/apps.py
+++ python/apps.py
@@ -1,4 +1,10 @@
 import mysql.connector
+import flask_cors
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+
 db=mysql.connector.connect(
        host="localhost",
        user="tu_usuario",
